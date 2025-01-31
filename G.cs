6dcc0e9d86f273f34b3a9c1b644425bc54dd804a
wellinton de Souza <168542172+wellintondsouza@users.noreply.github.com>
2025-01-31 08:48:57 -0300

    Create G.cs

---
+++ G.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        // Verificando se o número é positivo, negativo ou zero com operador ternário
+        string resultado = (numero > 0) ? "Positivo" : (numero < 0) ? "Negativo" : "Zero";
+
+        Console.WriteLine($"O número {numero} é {resultado}.");
+    }
+}
