efcd109de371d785748ba7500d3a7245cf2f777c
wellinton de Souza <168542172+wellintondsouza@users.noreply.github.com>
2025-01-31 08:40:01 -0300

    Create E.cs

---
+++ E.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: ");
+        int numero = int.Parse(Console.ReadLine());
+      
+        string resultado = (numero > 10) ? "A condição é verdadeira." : "A condição é falsa.";
+
+        Console.WriteLine(resultado);
+    }
+}
