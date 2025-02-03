f70cbe1656e80aec161be34bf2a1aad9218f7b7c
wellinton de Souza <168542172+wellintondsouza@users.noreply.github.com>
2025-02-03 08:08:55 -0300

    Update B.cs

--- B.cs
+++ B.cs
@@ -3,16 +3,25 @@ using System;
 class Program
 {
     static void Main()
+    
     {
-        Console.Write("Digite o primeiro número: ");
-        int numero1 = int.Parse(Console.ReadLine());
+        try
+        {
+            Console.Write("Digite o primeiro número: ");
+            int numero1 = int.Parse(Console.ReadLine());
 
-        Console.Write("Digite o segundo número: ");
-        int numero2 = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            int numero2 = int.Parse(Console.ReadLine());
 
-        // Encontrando o maior número com operador ternário
-        int maior = (numero1 > numero2) ? numero1 : numero2;
+            // Encontra o número com operador ternário
+            int maior = (numero1 > numero2) ? numero1 : numero2;
 
-        Console.WriteLine($"O maior número é: {maior}");
+            Console.WriteLine("O maior número é: {0}", maior);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Por favor, digite um número válido.");
+        }
+
+        Console.ReadKey();
     }
-}
