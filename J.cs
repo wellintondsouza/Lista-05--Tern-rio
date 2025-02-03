0e4feac5b2db2b28cd432bfa4934e10842dfb76a
wellinton de Souza <168542172+wellintondsouza@users.noreply.github.com>
2025-02-03 09:26:11 -0300

    Update J.cs

--- J.cs
+++ J.cs
@@ -4,11 +4,12 @@ class Program
 {
     static void Main()
     {
-        string texto = null;  // Você pode mudar esse valor para testar
+        string texto = null;  // Podemos mudar o valor para testar e assim o sistema identifica que a variável não é nula. string texto = "Palavra";
 
         // Verificando se a variável é nula com operador ternário
         string resultado = (texto == null) ? "A variável é nula." : "A variável não é nula.";
 
         Console.WriteLine(resultado);
+        Console.ReadKey();
     }
 }
